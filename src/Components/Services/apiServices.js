7baa6f3779ab7d85b9e7abbeef7417900a79eeb3
Vladyslaw Makhnyk <itdelive@gmail.com>
2021-10-20 14:48:41 +0300

    V-1.1

--- src/Components/Services/apiServices.js
+++ src/Components/Services/apiServices.js
@@ -2,7 +2,7 @@ import axios from "axios";
 axios.defaults.baseURL = "https://pixabay.com/api";
 const API_KEY = "23035715-e0cb760b48ddc49b0eff80b6c";
 
-function getPictures(query, page) {
+function getPictures({ query, page }) {
   return axios
     .get(
       `/?image_type=photo&orientation=horizontal&q=${query}&page=${page}&per_page=12&key=${API_KEY}`
